cea87b11948a79ee04544df86412b952f6735ebc
Gary Pang <gary.pang@gmail.com>
2016-08-29 23:20:37 -0400

    Create server, models, routes
    Create server with Node.js and Express.js. Make model with Mongoose (a
    Node.js library). Create API routes.

---
+++ app/routes/book.js
@@ -0,0 +1,68 @@
+let mongoose = require('mongoose');
+let Book = require('../models/book');
+
+/*
+GET /book route to retrieve all books
+ */
+function getBooks(req, res) {
+	// Query DB. If no errors, send all books
+	let query = Book.find({});
+	query.exec((err, books) => {
+		if (er) res.send(err);
+		// if no errors, send books back to client
+		res.json(books);
+	});
+}
+
+/*
+POST /book to save new book
+ */
+function postBook(req, res) {
+	// Creates new book
+	var newBook = new Book(req.body);
+	// Save it to DB
+	newBook.save((err, book) => {
+		if (err) {
+			res.send(err);
+		}
+		else { // If no errors, send book back to client
+			res.json({ message: "Book successfully added!", book });
+		}
+	});
+}
+
+/*
+GET /book/:id route to retrieve a book given its id
+ */
+function getBook(req, res) {
+	Book.findById(req.params.id, (err, book) => {
+		if (err) res.send(err);
+		// If no errors, send book back to client
+		res.json(book);
+	});
+}
+
+/*
+DELETE /book/:id to delete a book given its id
+ */
+function deleteBook(req, res) {
+	Book.remove({_id: req.params.id}, (err, result) => {
+		res.json({ message: "Book successfully deleted!", result });
+	});
+}
+
+/*
+PUT /book/:id to update a book given its id
+ */
+function updateBook(req, res) {
+	Book.findById({_id: req.params.id}, (err, book) => {
+		if (err) res.send(err);
+		Object.assign(book, req.body).save((err, book) => {
+			if (err) res.send(err);
+			res.json({ message: 'Book updated!', book });
+		});
+	});
+}
+
+// export all functions
+module.exports = { getBooks, postBook, getBook, deleteBook, updateBook };
---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8080;
+let book = require('./app/routes/book');
+let config = require('config'); // load db location from JSON files
+
+// db options
+let options = {
+				server: { socketOption: { keepAlive: 1, connectTimeoutMS: 30000 }},
+				replset: { socketOption: { keepAlive: 1, connectTimeoutMS: 30000 }}
+			  };
+
+// db connection
+mongoose.connect(config.DBHOST, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+	// use morgan to log at command line
+	app.use(morgan('combined')); // 'combined' outputs Apache style LOGs
+}
+
+// parse application/json and look for raw text
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.get("/", (req, res) => res.json({ message: "Welcome to our Bookstore!" }));
+
+app.route("/book")
+   .get(book.getBooks)
+   .post(book.postBook);
+
+app.route("/book/:id")
+   .get(book.getBook)
+   .delete(book.deleteBook)
+   .put(book.updateBook);
+
+app.listen(port);
+console.log("Listening on port " + port);
+
+module.exports = app; // exports server for testing
